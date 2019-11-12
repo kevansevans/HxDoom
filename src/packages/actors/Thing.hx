3709b7858826f69cead70560ad3fb01ed3c8c94c
Kaelan <kbeevans@gmail.com>
2019-11-12 12:38:50 -0700

    var setters as tutorial suggests

--- src/packages/actors/Thing.hx
+++ src/packages/actors/Thing.hx
@@ -9,13 +9,26 @@ package packages.actors;
 class Thing 
 {
 	public var id:Int;
-	public var xpos:Int;
-	public var ypos:Int;
-	public var angle:Int;
+	public var xpos(default, set):Int;
+	public var ypos(default, set):Int;
+	public var angle(default, set):Int;
 	
 	public function new(_id:Int) 
 	{
 		id = _id;
+	}
+	
+	function set_xpos(value:Int):Int 
+	{
+		return xpos = value;
+	}
+	function set_ypos(value:Int):Int 
+	{
+		return ypos = value;
+	}
+	function set_angle(value:Int):Int 
+	{
+		return angle = value;
 	}
 	
 }
