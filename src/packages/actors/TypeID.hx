f1056525d591bca212bcaa59d2a02a6826c16599
Kaelan <kbeevans@gmail.com>
2019-11-15 00:36:06 -0700

    Does not need to be public vars

--- src/packages/actors/TypeID.hx
+++ src/packages/actors/TypeID.hx
@@ -18,127 +18,127 @@ package packages.actors;
  */
 enum abstract TypeID(Int) from Int
 {
-	public var P_PLAYERONE:Int = 1; //= 1 initializes the enums correctly, each var further on incriments by one. If there's a jump in value, that needs to be explicitly set. See W_SHOTGUN below.
-	public var P_PLAYERTWO:Int; //= 2
-	public var P_PLAYERTHREE:Int; //= 3
-	public var P_PLAYERFOUR:Int; //= 4, etcetera
-	public var I_BLUEKEYCARD:Int;
-	public var I_YELLOWKEYCARD:Int;
-	public var M_SPIDERMASTERMIND:Int;
-	public var I_BACKPACK:Int;
-	public var M_FORMERSERGEANT:Int;
-	public var D_BLOODYMESS0:Int;
-	public var P_DEATHMATCHSTART:Int;
-	public var D_BLOODYMESS1:Int;
-	public var I_REDKEYCARD:Int;
-	public var T_TELEPORTLANDING:Int;
-	public var P_DEADPLAYER:Int;
-	public var M_CYBERDEMON:Int;
-	public var I_CELLCHARGEPACK:Int;
-	public var M_DEADFORMERHUMAN:Int;
-	public var M_DEADFORMERSERGEANT:Int;
-	public var M_DEADIMP:Int;
-	public var M_DEADDEMON:Int;
-	public var M_DEADCACODEMON:Int;
-	public var M_DEADLOSTSOUL:Int;
-	public var D_POOLOFBLOODANDFLESH:Int;
-	public var D_IMPALEDHUMAN:Int;
-	public var D_IMPALEDTWITCHINGHUMAN:Int;
-	public var D_SKULLONAPOLE:Int;
-	public var D_SKULLSHISHKEBAB:Int;
-	public var D_PILEOFSKULLANDCANDLES:Int;
-	public var D_TALLGREENPILLAR:Int;
-	public var D_SHORTGREENPILLAR:Int;
-	public var D_TALLREDPILLAR:Int;
-	public var D_SHORTREDPILLAR:Int;
-	public var D_CANDLE:Int;
-	public var D_CANDELABRA:Int;
-	public var D_SHORTGREENPILLARWITHHEART:Int;
-	public var D_SHORTREDPILLARWITHSKULL:Int;
-	public var I_BLUESKULLKEY:Int;
-	public var I_YELLOWSKULLKEY:Int;
-	public var I_REDSKULLKEY:Int;
-	public var D_EVILEYE:Int;
-	public var D_FLOATINGSKULL:Int;
-	public var D_BURNTTREE:Int;
-	public var D_TALLBLUEFIRESTICK:Int;
-	public var D_TALLGREENFIRESTICK:Int;
-	public var D_TALLREDFIRESTICK:Int;
-	public var D_STALAGMITE:Int;
-	public var D_TALLTECHNOPILLAR:Int;
-	public var D_HANGINGVICTIMTWITCHING0:Int;
-	public var D_HANGINGVICTIMARMSOUT0:Int;
-	public var D_HANGINGVICTIMONELEG0:Int;
-	public var D_HANGINGPAIROFLEGS0:Int;
-	public var D_HANGINGLEG0:Int;
-	public var D_LARGEBROWNTREE:Int;
-	public var D_SHORTBLUEFIRESTICK:Int;
-	public var D_SHORTGREENFIRESTICK:Int;
-	public var D_SHORTREDFIRESTICK:Int;
-	public var M_SPECTRE:Int;
-	public var D_HANGINGVICTIMARMSOUT1:Int;
-	public var D_HANGINGPAIROFLEGS1:Int;
-	public var D_HANGINGVICTIMONELEG1:Int;
-	public var D_HANGINGLEG1:Int;
-	public var D_HANGINGVICTIMTWITCHING1:Int;
-	public var M_ARCHVILE:Int;
-	public var M_FORMERCOMMANDO:Int;
-	public var M_REVENANT:Int;
-	public var M_MANCUBUS:Int;
-	public var M_ARACHNOTRON:Int;
-	public var M_HELLKNIGHT:Int;
-	public var D_BURNINGBARREL:Int;
-	public var M_PAINELEMENTAL:Int;
-	public var M_COMMANDERKEEN:Int;
-	public var D_HANGINGVICTIMGUTSREMOVED:Int;
-	public var D_HANGINGVICTIMGUTSANDBRAINREMOVED:Int;
-	public var D_HANGINGTORSOLOOKINGDOWN:Int;
-	public var D_HANGINGTORSOOPENSKULL:Int;
-	public var D_HANGINGTORSOLOOKINGUP:Int;
-	public var D_HANGINGVTORSOBRAINREMOVED:Int;
-	public var D_POOLOFBLOOD0:Int;
-	public var D_POOLOFBLOOD1:Int;
-	public var D_POOLOFBRAINS:Int;
-	public var W_SUPERSHOTGUN:Int;
-	public var I_MEGASPHERE:Int;
-	public var M_WOLFSS:Int;
-	public var D_TALLTECHNOFLOORLAMP:Int;
-	public var D_SHORTTECHNOFLOORLAMP:Int;
-	public var M_SPAWNSPOT:Int; //= 87
-	public var M_BOSSBRAIN:Int; //= 88
-	public var M_BOSSSHOOTER:Int; //= 89
-	public var W_SHOTGUN:Int = 2001; 
-	public var W_CHAINGUN:Int; //= 2002
-	public var W_ROCKETLAUNCHER:Int; //= 2003
-	public var W_PLASMARIFLE:Int; //= 2004, etcetera
-	public var W_CHAINSAW:Int;
-	public var W_BFK9000:Int;
-	public var I_AMMOCLIP:Int;
-	public var I_SHOTGUNSHELLS:Int;
-	public var I_ROCKET:Int;
-	public var I_STIMPACK:Int;
-	public var I_MEDIKIT:Int;
-	public var I_SOULSPHERE:Int;
-	public var I_HEALTHPOTION:Int;
-	public var I_SPIRITARMOR:Int;
-	public var I_GREENARMOR:Int;
-	public var I_BLUEARMOR:Int;
-	public var I_INVULNERABILITY:Int;
-	public var I_BERSERK:Int;
-	public var I_INVISIBILITY:Int;
-	public var I_RADIATIONSUIT:Int;
-	public var I_COMPUTERMAP:Int;
-	public var D_FLOORLAMP:Int;
-	public var D_BARREL:Int;
-	public var I_LIGHTAMPLIFICATIONVISOR:Int;
-	public var I_BOXOFROCKETS:Int;
-	public var I_CELLCHARGE:Int; //= 2047
-	public var I_BOXOFAMMO:Int; //= 2048
-	public var I_BOXOFSHELLS:Int; //=2049
-	public var M_IMP:Int = 3001;
-	public var M_DEMON:Int; //= 3002
-	public var M_BARONOFHELL:Int; //= 3003
-	public var M_FORMERTROOPER:Int; //= 3004, etcetera
-	public var M_CACODEMON:Int;
-	public var M_LOSTSOUL:Int;
+	var P_PLAYERONE:Int = 1; //= 1 initializes the enums correctly, each var further on incriments by one. If there's a jump in value, that needs to be explicitly set. See W_SHOTGUN below.
+	var P_PLAYERTWO:Int; //= 2
+	var P_PLAYERTHREE:Int; //= 3
+	var P_PLAYERFOUR:Int; //= 4, etcetera
+	var I_BLUEKEYCARD:Int;
+	var I_YELLOWKEYCARD:Int;
+	var M_SPIDERMASTERMIND:Int;
+	var I_BACKPACK:Int;
+	var M_FORMERSERGEANT:Int;
+	var D_BLOODYMESS0:Int;
+	var P_DEATHMATCHSTART:Int;
+	var D_BLOODYMESS1:Int;
+	var I_REDKEYCARD:Int;
+	var T_TELEPORTLANDING:Int;
+	var P_DEADPLAYER:Int;
+	var M_CYBERDEMON:Int;
+	var I_CELLCHARGEPACK:Int;
+	var M_DEADFORMERHUMAN:Int;
+	var M_DEADFORMERSERGEANT:Int;
+	var M_DEADIMP:Int;
+	var M_DEADDEMON:Int;
+	var M_DEADCACODEMON:Int;
+	var M_DEADLOSTSOUL:Int;
+	var D_POOLOFBLOODANDFLESH:Int;
+	var D_IMPALEDHUMAN:Int;
+	var D_IMPALEDTWITCHINGHUMAN:Int;
+	var D_SKULLONAPOLE:Int;
+	var D_SKULLSHISHKEBAB:Int;
+	var D_PILEOFSKULLANDCANDLES:Int;
+	var D_TALLGREENPILLAR:Int;
+	var D_SHORTGREENPILLAR:Int;
+	var D_TALLREDPILLAR:Int;
+	var D_SHORTREDPILLAR:Int;
+	var D_CANDLE:Int;
+	var D_CANDELABRA:Int;
+	var D_SHORTGREENPILLARWITHHEART:Int;
+	var D_SHORTREDPILLARWITHSKULL:Int;
+	var I_BLUESKULLKEY:Int;
+	var I_YELLOWSKULLKEY:Int;
+	var I_REDSKULLKEY:Int;
+	var D_EVILEYE:Int;
+	var D_FLOATINGSKULL:Int;
+	var D_BURNTTREE:Int;
+	var D_TALLBLUEFIRESTICK:Int;
+	var D_TALLGREENFIRESTICK:Int;
+	var D_TALLREDFIRESTICK:Int;
+	var D_STALAGMITE:Int;
+	var D_TALLTECHNOPILLAR:Int;
+	var D_HANGINGVICTIMTWITCHING0:Int;
+	var D_HANGINGVICTIMARMSOUT0:Int;
+	var D_HANGINGVICTIMONELEG0:Int;
+	var D_HANGINGPAIROFLEGS0:Int;
+	var D_HANGINGLEG0:Int;
+	var D_LARGEBROWNTREE:Int;
+	var D_SHORTBLUEFIRESTICK:Int;
+	var D_SHORTGREENFIRESTICK:Int;
+	var D_SHORTREDFIRESTICK:Int;
+	var M_SPECTRE:Int;
+	var D_HANGINGVICTIMARMSOUT1:Int;
+	var D_HANGINGPAIROFLEGS1:Int;
+	var D_HANGINGVICTIMONELEG1:Int;
+	var D_HANGINGLEG1:Int;
+	var D_HANGINGVICTIMTWITCHING1:Int;
+	var M_ARCHVILE:Int;
+	var M_FORMERCOMMANDO:Int;
+	var M_REVENANT:Int;
+	var M_MANCUBUS:Int;
+	var M_ARACHNOTRON:Int;
+	var M_HELLKNIGHT:Int;
+	var D_BURNINGBARREL:Int;
+	var M_PAINELEMENTAL:Int;
+	var M_COMMANDERKEEN:Int;
+	var D_HANGINGVICTIMGUTSREMOVED:Int;
+	var D_HANGINGVICTIMGUTSANDBRAINREMOVED:Int;
+	var D_HANGINGTORSOLOOKINGDOWN:Int;
+	var D_HANGINGTORSOOPENSKULL:Int;
+	var D_HANGINGTORSOLOOKINGUP:Int;
+	var D_HANGINGVTORSOBRAINREMOVED:Int;
+	var D_POOLOFBLOOD0:Int;
+	var D_POOLOFBLOOD1:Int;
+	var D_POOLOFBRAINS:Int;
+	var W_SUPERSHOTGUN:Int;
+	var I_MEGASPHERE:Int;
+	var M_WOLFSS:Int;
+	var D_TALLTECHNOFLOORLAMP:Int;
+	var D_SHORTTECHNOFLOORLAMP:Int;
+	var M_SPAWNSPOT:Int; //= 87
+	var M_BOSSBRAIN:Int; //= 88
+	var M_BOSSSHOOTER:Int; //= 89
+	var W_SHOTGUN:Int = 2001; 
+	var W_CHAINGUN:Int; //= 2002
+	var W_ROCKETLAUNCHER:Int; //= 2003
+	var W_PLASMARIFLE:Int; //= 2004, etcetera
+	var W_CHAINSAW:Int;
+	var W_BFK9000:Int;
+	var I_AMMOCLIP:Int;
+	var I_SHOTGUNSHELLS:Int;
+	var I_ROCKET:Int;
+	var I_STIMPACK:Int;
+	var I_MEDIKIT:Int;
+	var I_SOULSPHERE:Int;
+	var I_HEALTHPOTION:Int;
+	var I_SPIRITARMOR:Int;
+	var I_GREENARMOR:Int;
+	var I_BLUEARMOR:Int;
+	var I_INVULNERABILITY:Int;
+	var I_BERSERK:Int;
+	var I_INVISIBILITY:Int;
+	var I_RADIATIONSUIT:Int;
+	var I_COMPUTERMAP:Int;
+	var D_FLOORLAMP:Int;
+	var D_BARREL:Int;
+	var I_LIGHTAMPLIFICATIONVISOR:Int;
+	var I_BOXOFROCKETS:Int;
+	var I_CELLCHARGE:Int; //= 2047
+	var I_BOXOFAMMO:Int; //= 2048
+	var I_BOXOFSHELLS:Int; //=2049
+	var M_IMP:Int = 3001;
+	var M_DEMON:Int; //= 3002
+	var M_BARONOFHELL:Int; //= 3003
+	var M_FORMERTROOPER:Int; //= 3004, etcetera
+	var M_CACODEMON:Int;
+	var M_LOSTSOUL:Int;
 }
