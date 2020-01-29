09a2322c69245ad21ce12cf1b3c4daa862f33913
Kaelan <kbeevans@gmail.com>
2020-01-29 00:56:40 -0700

    Big ol' comment
    because apparently macros are magic and can be difficult to understand

--- src/hxdoom/com/CheatHandler.hx
+++ src/hxdoom/com/CheatHandler.hx
@@ -40,47 +40,85 @@ class CheatHandler
 	 */
 	
 	public static macro function checkCheat() {
-		var switch_block:Array<String> = new Array();
-		
-		//DO NOT TOUCH ANY OF THIS BELOW
-		switch_block = 	['{',
-			'var log = keylogger.copy();',
-			'log.reverse();',
-			'switch (log) {',
-				'default :',
-					'doNothing();',
-			'}',
-		'}'];
-		//DO NOT TOUCH ANY OF THIS ABOVE
-		
-		var file = File.getBytes("./assets/json/cheats.txt");
-		var codesheet:Dynamic = Json.parse(file.getString(0, file.length));
-		var currentgame:Dynamic;
-		var currentcheat:Dynamic;
-		
-		for (game in Reflect.fields(codesheet)) {
-			
-			currentgame = Reflect.getProperty(codesheet, game);
-			
-			for (cheatcode in Reflect.fields(currentgame)) {
-				
-				currentcheat = Reflect.getProperty(currentgame, cheatcode);
-				
-				var caseheader = 'case [';
-				var length:Int = 20 - Std.int(cheatcode.length);
-				for (a in 0...length) {
-					caseheader += '_, ';
-				}
-				for (a in 0...cheatcode.length) {
-					caseheader += '"' + cheatcode.substr(a, 1) + '", ';
-				}
-				caseheader = caseheader.substring(0, caseheader.length - 2);
-				caseheader += '] :';
-				
-				var actionblock:String = '';
-				
-				var scripts:Array<Dynamic> = cast(currentcheat, Array<Dynamic>);
-				
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	//DO NOT TOUCH ANY OF THIS BELOW
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	/**/
+	/**/	var switch_block:Array<String> = new Array();												
+	/**/	
+	/**/	switch_block = 	['{',
+	/**/		'var log = keylogger.copy();',
+	/**/		'log.reverse();',
+	/**/		'switch (log) {',
+	/**/			'default :',
+	/**/				'doNothing();',
+	/**/		'}',
+	/**/	'}'];
+	/**
+	 * 		Okay, touch it if you really have to after THIS point. The below code is set up
+	 * 		to parse through a JSON file where the field names are completely unknown, but assumed
+	 * 		to hold a very specific structure and is correctly formatted to the JSON standard.
+	 * 		the structure is as follows:
+	 * 
+	 * 		associated game -> key combo -> conditions to apply
+	 * 
+	 *		{
+	 *			"doom": {
+	 *				"iddqd": [
+	 *					"pseudoinvul"
+	 *				]
+	 * 			}
+	 * 		}
+	 * 
+	 * 		the parser below will turn the above JSON into the following two strings:
+	 * 		
+	 * 			case [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, "i", "d", "d", "q", "d"] :
+	 * 
+	 *		and... 
+	 * 		
+	 * 			GameActions.cheat_degreeless();
+	 * 
+	 * 		These are in the correct syntax that haxe expects. It then inserts those two strings
+	 * 		into the array above. The array above is very delicate, do not touch it. Once the array is
+	 * 		finished, the Haxe macro compiler will then turn it into valid Haxe code and inline it into where
+	 * 		checkCheat(); happens above. This code is precompiled before the main program is compiled,
+	 * 		this class WILL NOT hotload cheats into the engine, this is to provide an easy API for
+	 * 		anyone on the dev side of things to easily modify cheat code behavior.
+	 * 
+	 */		var file = File.getBytes("./assets/json/cheats.txt");
+	/**/	var codesheet:Dynamic = Json.parse(file.getString(0, file.length));
+	/**/	var currentgame:Dynamic;
+	/**/	var currentcheat:Dynamic;
+	/**/	
+	/**/	for (game in Reflect.fields(codesheet)) {
+	/**/		
+	/**/		currentgame = Reflect.getProperty(codesheet, game);
+	/**/		
+	/**/		for (cheatcode in Reflect.fields(currentgame)) {
+	/**/			
+	/**/			currentcheat = Reflect.getProperty(currentgame, cheatcode);
+	/**/			
+	/**/			var caseheader = 'case [';
+	/**/			var length:Int = 20 - Std.int(cheatcode.length); //<- Only touch this number if `keylogger` array also changes
+	/**/			for (a in 0...length) {
+	/**/				caseheader += '_, ';
+	/**/			}
+	/**/			for (a in 0...cheatcode.length) {
+	/**/				caseheader += '"' + cheatcode.substr(a, 1) + '", ';
+	/**/			}
+	/**/			caseheader = caseheader.substring(0, caseheader.length - 2);
+	/**/			caseheader += '] :';
+	/**/			
+	/**/			var actionblock:String = '';
+	/**/			
+	/**/			var scripts:Array<Dynamic> = cast(currentcheat, Array<Dynamic>);
+	/**/			
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	//DO NOT TOUCH ANY OF THIS ABOVE
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	//This part might turn into a macro itself. Who knows.
 				for (actions in scripts) {
 					var actionstring:String = cast(actions, String);
 					switch (cast(actions, String)) {
@@ -94,7 +132,7 @@ class CheatHandler
 					}
 				}
 				
-				switch_block.insert(4, caseheader);
+				switch_block.insert(4, caseheader); //<-- Don't change these integers what so ever.
 				switch_block.insert(5, actionblock);
 			}
 		}
@@ -103,6 +141,5 @@ class CheatHandler
 		
 		return Context.parse(code, Context.currentPos());
 	}
-	//cheat functions
 	public function doNothing(){};
 }
