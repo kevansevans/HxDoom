bbeb6e6e652e85cd945fb0212ffcd5e8e315b2eb
Kaelan <kbeevans@gmail.com>
2020-02-23 09:31:19 -0700

    Need more reliable way to find sector

--- src/hxdoom/data/maplumps/SubSector.hx
+++ src/hxdoom/data/maplumps/SubSector.hx
@@ -7,12 +7,14 @@ package hxdoom.data.maplumps;
 class SubSector 
 {
 	public var segments:Array<Segment>;
+	public var sector:Sector;
 	public function new(_segments:Array<Segment>, _count:Int, _id:Int) 
 	{
 		segments = new Array();
 		for (_seg in _id...(_id + _count)) {
 			segments.push(_segments[_seg]);
 		}
+		sector = segments[0].direction == 0 ? segments[0].frontSector : segments[0].backSector;
 	}
 	
 }
