7dadc1d92f246c9cb0bd7e17b3c3da22a0477827
Kaelan <kbeevans@gmail.com>
2020-04-02 11:15:10 -0600

    Start of default values for doomwaddir

---
+++ src/hxdoom/enums/WadDirectory.hx
@@ -0,0 +1,10 @@
+package hxdoom.enums;
+
+/**
+ * @author Kaelan
+ */
+enum abstract WadDirectory(String) from String
+{
+	var windows:String = "C:/DOOM/WADS";
+	var linux:String = "/usr/local/share/games/DOOM/WADS";
+}
