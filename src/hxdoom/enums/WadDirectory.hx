f6439c58e0c28550fd4564aa717305290c383e89
Kaelan <kbeevans@gmail.com>
2020-04-02 11:35:34 -0600

    Correct linux notation

--- src/hxdoom/enums/WadDirectory.hx
+++ src/hxdoom/enums/WadDirectory.hx
@@ -6,5 +6,5 @@ package hxdoom.enums;
 enum abstract WadDirectory(String) from String
 {
 	var windows:String = "C:/DOOM/WADS";
-	var linux:String = "/usr/local/share/games/DOOM/WADS";
+	var linux:String = "~/usr/local/share/games/DOOM/WADS";
 }
