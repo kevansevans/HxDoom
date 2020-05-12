ea8601f4fc01ee9e9752b0184d58ea20965a3c9d
Kaelan <kbeevans@gmail.com>
2020-05-12 13:17:37 -0600

    [HxDoom] Dedicated HXDKeyCode file

--- src/hxdoom/core/IOCore.hx
+++ src/hxdoom/core/IOCore.hx
@@ -3,6 +3,7 @@ package hxdoom.core;
 import hxdoom.core.CheatCore;
 import hxdoom.core.GameCore;
 import hxdoom.utils.enums.Defaults;
+import hxdoom.utils.enums.HXDKeyCode;
 
 /**
  * ...
@@ -107,108 +108,4 @@ class IOCore
 	{
 		return 0;
 	}
-}
-
-enum abstract HXDKeyCode(Int) from Int {
-	var NULL:Int = 0x0;
-	var BACKSPACE:Int = 0x08;
-	var TAB:Int = 0x09;
-	var ESCAPE:Int = 0x1B;
-	var SPACE:Int = 0x20;
-	//Going to rely on implicit casting from here on
-	var EXCLAMATION:Int = 0x21;
-	var QUOTE:Int;
-	var OCTOTHORPE:Int;
-	var DOLLAR:Int;
-	var PERCENT:Int;
-	var AMPERSAND:Int;
-	var APOSTROPHE:Int;
-	var OPEN_PARENTHESIS:Int;
-	var CLOSE_PARENTHESIS:Int;
-	var ASTERISK:Int;
-	var PLUS:Int;
-	var ACUTE_DIACRITIC:Int;
-	var HYPHEN:Int;
-	var PERIOD:Int;
-	var FOWARD_SLASH:Int;
-	var ZERO:Int;
-	var ONE:Int;
-	var TWO:Int;
-	var THREE:Int;
-	var FOUR:Int;
-	var FIVE:Int;
-	var SIX:Int;
-	var SEVEN:Int;
-	var EIGHT:Int;
-	var NINE:Int;
-	var COLON:Int;
-	var SEMICOLON:Int;
-	var LESSTHAN:Int;
-	var EQUALS:Int;
-	var GREATERTHAN:Int;
-	var QUESTION:Int;
-	var AT:Int;
-	var A_UPPER:Int;
-	var B_UPPER:Int;
-	var C_UPPER:Int;
-	var D_UPPER:Int;
-	var E_UPPER:Int;
-	var F_UPPER:Int;
-	var G_UPPER:Int;
-	var H_UPPER:Int;
-	var I_UPPER:Int;
-	var J_UPPER:Int;
-	var K_UPPER:Int;
-	var L_UPPER:Int;
-	var M_UPPER:Int;
-	var N_UPPER:Int;
-	var O_UPPER:Int;
-	var P_UPPER:Int;
-	var Q_UPPER:Int;
-	var R_UPPER:Int;
-	var S_UPPER:Int;
-	var T_UPPER:Int;
-	var U_UPPER:Int;
-	var V_UPPER:Int;
-	var W_UPPER:Int;
-	var X_UPPER:Int;
-	var Y_UPPER:Int;
-	var Z_UPPER:Int;
-	var OPEN_BRACKET:Int;
-	var BACK_SLASH:Int;
-	var CLOSE_BRACKET:Int;
-	var CARET:Int;
-	var UNDERSCORE:Int;
-	var GRAVE_DIACRITIC:Int;
-	var A_LOWER:Int;
-	var B_LOWER:Int;
-	var C_LOWER:Int;
-	var D_LOWER:Int;
-	var E_LOWER:Int;
-	var F_LOWER:Int;
-	var G_LOWER:Int;
-	var H_LOWER:Int;
-	var I_LOWER:Int;
-	var J_LOWER:Int;
-	var K_LOWER:Int;
-	var L_LOWER:Int;
-	var M_LOWER:Int;
-	var N_LOWER:Int;
-	var O_LOWER:Int;
-	var P_LOWER:Int;
-	var Q_LOWER:Int;
-	var R_LOWER:Int;
-	var S_LOWER:Int;
-	var T_LOWER:Int;
-	var U_LOWER:Int;
-	var V_LOWER:Int;
-	var W_LOWER:Int;
-	var X_LOWER:Int;
-	var Y_LOWER:Int;
-	var Z_LOWER:Int;
-	var OPEN_BRACE:Int;
-	var PIPE:Int;
-	var CLOSE_BRACE:Int;
-	var TILDE:Int;
-	var DEL:Int;
 }
