e0b2821ee76ba96960b5ef403c8ff853d3baf182
Kaelan <kbeevans@gmail.com>
2020-06-04 14:09:48 -0600

    [HxDoom] Angle.hx documentation for Dox

--- library/src/hxdoom/utils/geom/Angle.hx
+++ library/src/hxdoom/utils/geom/Angle.hx
@@ -1,85 +1,94 @@
 package hxdoom.utils.geom;
 
 /**
- * ...
+ * Abstract Haxe float that treats it's value as a 360 degree circle. Going over 360 will overflow back to 0, and going below 0 will underflow back to 360.
+ * Treat as Haxe float otherwise.
  * @author Kaelan
  */
 abstract Angle(Float) to Float from Float from Int
 {
-	public inline function new(_v:Float) {
+	@:dox(hide) public inline function new(_v:Float) {
 		this = _v;
 	}
-	@:noCompletion @:op(A + B)
+	@:dox(hide) @:op(A + B)
 	public inline function add(B:Float) {
 		return adjust(this + B);
 	}
-	@:op(A += B)
+	@:dox(hide) @:op(A += B)
 	public inline function addeq(B:Float) {
 		return adjust(this += B);
 	}
-	@:op(A - B)
+	@:dox(hide) @:op(A - B)
 	public inline function sub(B:Float) {
 		return adjust(this - B);
 	}
-	@:op(A -= B)
+	@:dox(hide) @:op(A -= B)
 	public inline function subeq(B:Float) {
 		return adjust(this - B);
 	}
-	@:op(A * B)
+	@:dox(hide) @:op(A * B)
 	public inline function mult(B:Float) {
 		return adjust(this * B);
 	}
-	@:op(A *= B)
+	@:dox(hide) @:op(A *= B)
 	public inline function multeq(B:Float) {
 		return adjust(this *= B);
 	}
-	@:op(A / B)
+	@:dox(hide) @:op(A / B)
 	public inline function div(B:Float) {
 		return adjust(this / B);
 	}
-	@:op(A /= B)
+	@:dox(hide) @:op(A /= B)
 	public inline function diveq(B:Float) {
 		return adjust(this /= B);
 	}
-	@:op(++A)
+	@:dox(hide) @:op(++A)
 	public inline function preinc() {
 		return this;
 	}
-	@:op(A++)
+	@:dox(hide) @:op(A++)
 	public inline function postinc() {
 		return adjust(this++);
 	}
-	@:op(--A)
+	@:dox(hide) @:op(--A)
 	public inline function predec() {
 		return this;
 	}
-	@:op(A--)
+	@:dox(hide) @:op(A--)
 	public inline function postdec() {
 		return adjust(this--);
 	}
-	@:op(A >= B)
+	@:dox(hide) @:op(A >= B)
 	public inline function greaterThanAndEqual(_b:Float) {
 		return (this > _b || this == _b);
 	}
-	@:op(A <= B)
+	@:dox(hide) @:op(A <= B)
 	public inline function lessThanAndEqual(_b:Float) {
 		return (this < _b || this == _b);
 	}
-	@:op(A > B)
+	@:dox(hide) @:op(A > B)
 	public inline function greaterThan(_b:Float) {
 		return (this > _b);
 	}
-	@:op(A < B)
+	@:dox(hide) @:op(A < B)
 	public inline function lessThan(_b:Float) {
 		return (this < _b);
 	}
+	/**
+	 * Returns itself as a Float instead of Angle.
+	 * @return
+	 */
 	public inline function asValue():Float {
 		return cast(this, Float);
 	}
+	/**
+	 * Returns itself as a Float and converts to Radians.
+	 * @return
+	 */
 	public inline function toRadians():Float {
 		return (this * (Math.PI / 180));
 	}
-	static inline function adjust(_v:Float):Float {
+	@:dox(hide) static inline function adjust(_v:Float):Float {
 		if (_v > 360) _v -= 360;
 		if (_v < 0) _v += 360;
 		return _v;
