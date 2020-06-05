dd74c188793e52f16cd87747401ae55c9c66e1f2
Kaelan <kbeevans@gmail.com>
2020-06-05 14:34:31 -0600

    [HxDoom] Data lumps
    For lumps who's namespace doesn't indicate reading behavior.

---
+++ library/src/hxdoom/enums/eng/DataLump.hx
@@ -0,0 +1,14 @@
+package hxdoom.enums.eng;
+
+/**
+ * Lumps whos name does not directly indicate their intended purpose.
+ * @author Kaelan
+ */
+enum abstract DataLump(String) from String 
+{
+	var GRAPHIC:String;
+	var SOUNDFX:String;
+	var SOUNDPC:String;
+	var MIDIMUS:String;
+	var UNKNOWN:String;
+}
