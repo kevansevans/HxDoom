7de8c2a65c0d9b05a32c113d71c9f4065a6fe704
Kaelan <kbeevans@gmail.com>
2020-08-16 14:10:11 -0600

    [HxDoom] Revert this for dox gen safety
    also this part ain't ready either

--- library/src/hxdoom/core/ActorCore.hx
+++ library/src/hxdoom/core/ActorCore.hx
@@ -9,18 +9,8 @@ import hxdoom.utils.geom.Angle;
  */
 class ActorCore 
 {
-	public static var ActorList:Array<Void -> Void>
-	public static var ActiveList:Array<Actor>;
-	
-	public static function Summon(_actorID:Int, _x:Float = 0, _y:Float = 0, _angle:Angle = 0) {
-		if (ActorList[_actorID] == null) {
-			ActiveList.push(Actor.CONSTRUCTOR([]);
-		}
-	}
-	
 	public function new() 
 	{
 		
-	}
-	
+	}	
 }
