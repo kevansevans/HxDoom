64f3c4ea03fd7a3fe45d5368e2a544d574ccad18
Kaelan <kbeevans@gmail.com>
2020-09-09 14:22:52 -0600

    [HxDoom] Removal of old Cardinal Direction enum

--- library/src/hxdoom/lumps/map/Thing.hx
+++ library/src/hxdoom/lumps/map/Thing.hx
@@ -1,8 +1,6 @@
 package hxdoom.lumps.map;
 
 import haxe.io.Bytes;
-import hxdoom.enums.eng.Direction.CardInt;
-import hxdoom.enums.eng.Direction.CardString;
 import hxdoom.lumps.LumpBase;
 
 /**
@@ -33,36 +31,11 @@ class Thing extends LumpBase
 	
 	public function toString():String {
 		return([	'Position {x :' + xpos + ', y: ' + ypos + '}, ',
-					'Direction {Angle: ' + angle + ', Cardinal: '  + cardinality(angle) + '}, ',
+					'Direction {Angle: ' + angle + '}, ',
 					'Type: {ThingID: ' + type + '}, ',
 					'Flags: {' + flags + '}'
 		
 		].join(""));
-	}
-	
-	public function cardinality(_angle:Int):String {
-		switch (_angle) {
-			case CardInt.EAST | CardInt.EAST2:
-				return CardString.EAST;
-			case CardInt.NORTHEAST:
-				return CardString.NORTHEAST;
-			case CardInt.NORTH:
-				CardString.NORTH;
-			case CardInt.NORTHWEST:
-				return CardString.NORTHWEST;
-			case CardInt.WEST:
-				return CardString.WEST;
-			case CardInt.SOUTHWEST:
-				return CardString.SOUTHWEST;
-			case CardInt.SOUTH: 
-				return CardString.SOUTH;
-			case CardInt.SOUTHEAST:
-				return CardString.SOUTHEAST;
-			default:
-				return "Corrupt Direction, this should NEVER happen";
-		}
-		
-		return "Corrupt Direction, this should NEVER happen. This is only here so the compiler doesn't freak out, BUT STILL SHOULD NEVER HAPPEN: " + angle;
 	}
 	
 	override public function toDataBytes():Bytes 
