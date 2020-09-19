2c955a43dbd60835d257b385e818bf9bdf13f13b
Kaelan <kbeevans@gmail.com>
2020-09-19 14:07:48 -0600

    [HxDoom] Hexen linedef lump

---
+++ library/src/hxdoom/lumps/map/LineDefHexen.hx
@@ -0,0 +1,31 @@
+package hxdoom.lumps.map;
+
+/**
+ * ...
+ * @author Kaelan
+ */
+class LineDefHexen extends LineDef 
+{
+	public static inline var BYTE_SIZE:Int = 16;
+	
+	public static var CONSTRUCTOR:(Array<Any>) -> LineDefHexen = LineDefHexen.new;
+	
+	public var arg_1:Int;
+	public var arg_2:Int;
+	public var arg_3:Int;
+	public var arg_4:Int;
+	public var arg_5:Int;
+	
+	public function new(_args:Array<Any>) 
+	{
+		super([_args[0], _args[1], _args[2], _args[3], 0, _args[9], _args[10]]);
+		
+		arg_1 = _args[4];
+		arg_2 = _args[5];
+		arg_3 = _args[6];
+		arg_4 = _args[7];
+		arg_5 = _args[8];
+		
+	}
+	
+}
