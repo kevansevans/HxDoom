6b785feb22f7de300523b73292eb7ab1bc9774e3
Kaelan <kbeevans@gmail.com>
2020-12-08 11:25:48 -0700

    [HxDoom] Linedef properties defined
    Since these can be mix and matched, the value is better suited as a typedef instead of enum.

--- HxDoom-CoreLib/src/hxdoom/typedefs/properties/LineDefProperties.hx
+++ HxDoom-CoreLib/src/hxdoom/typedefs/properties/LineDefProperties.hx
@@ -5,5 +5,13 @@ package;
  */
 typedef LineDefProperties =
 {
-	
+	@:optional var blocking:Bool;
+	@:optional var blockMonsters:Bool;
+	@:optional var twoSided:Bool;
+	@:optional var dontPegTop:Bool;
+	@:optional var dontPegBottom:Bool;
+	@:optional var secret:Bool;
+	@:optional var soundBlock:Bool;
+	@:optional var dontDraw:Bool;
+	@:optional var mapped:Bool;
 }
